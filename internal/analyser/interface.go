3b9072158ea9d864460692447d21f96d43b57a36
Chrusty <>
2022-06-20 17:37:42 +1200

    Naming and comments

--- internal/analyser/interface.go
+++ internal/analyser/interface.go
@@ -1,6 +1,8 @@
 package analyser
 
+// Analyse go modules / imports, and analyse the changes to see if anything needs to be rebuilt:
 type Analyser interface {
+	AnalyseChanges(changedFiles []string, importedPackages ImportedPackages) (changesDetected bool)
 	GetPackages(buildPackage string) (ImportedPackages, error)
 	GetRootModule() (string, error)
 }
